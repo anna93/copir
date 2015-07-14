0c017c658e5202ffd7735234f3cbea398f55c12d
anna93 <shekharanna1993@gmail.com>
2015-07-15 01:01:17 +0530

    added select all functionality

--- pj/scripts/copy.js
+++ pj/scripts/copy.js
@@ -1,3 +1,5 @@
+var inputFileVal = '';
+
 function updateDropDown() {
 	var saveVal = false;
 	if($("#profile-selector option:selected").length == 1) {
@@ -22,9 +24,69 @@ function fillFileInputs() {
 	$("#file-input-common").trigger("click");
 	var context = this;
 	$("#file-input-common").on("change",function() {
-		$(context).val($("#file-input-common").val());			
+		inputFileVal = $("#file-input-common").val();
+		$(context).val(inputFileVal);	
+		populateLeft();	
 	});
 }
+
+function populateLeft() {
+	var content = '';
+	fs.readFile(inputFileVal,'utf8',function(err,data) {
+		if (err) {
+			throw err;
+			alert("Could not open file, try again.");
+		}
+	    if(data != "") {
+		    content = data;		    
+		    content = content.split("\n");
+		    populateLeftHelper(content);
+		    // console.log(content);
+		}
+	});
+}
+
+function populateLeftHelper(content) {
+	$("#selector-left, #selector-right").html();
+	var rows = '';
+	rows += '<div><input id="select-all-left" type="checkbox">&nbsp;<label style="direction:ltr">Select all</label><div class="clearfix"></div></div>';
+
+	$(content).each(function() {
+		rows += '<div title="'+this+'"><input class="cbk-left" type="checkbox">&nbsp;<label>'+this+'</label><div class="clearfix"></div></div>';		
+	});
+
+	$("#selector-left").html(rows);
+}
+
+
+$('body').on('change','#select-all-left',function() {
+	if($(this).is(":checked")) {
+		$(this).parent().css("background-color","#333");
+		$("#selector-left .cbk-left").each(function() {
+			$(this).prop("checked",true);
+			$(this).parent().css("background-color","#333");
+		});
+	}
+	else {
+		$(this).parent().css("background-color","#101010");
+		$("#selector-left .cbk-left").each(function() {
+			$(this).prop("checked",false);
+			$(this).parent().css("background-color","#101010");
+		});
+	}
+})
+
+$('body').on('change','.cbk-left',function() {
+	if($(this).is(":checked")) {
+		$(this).parent().css("background-color","#333");
+	}
+	else {
+		$(this).parent().css("background-color","#101010");
+		$("#select-all-left").prop("checked",false);
+		$("#select-all-left").parent().css("background-color","#101010");
+	}
+});
+
 
 $("#profile-selector").mouseenter(updateDropDown);
 $("#input-file-txt").click(fillFileInputs);
